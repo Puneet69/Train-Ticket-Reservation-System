a28c7c36530964e55d831323164511a0a2db12e7
Puneet69 <puneetgupta11459282@gmail.com>
2025-11-16 17:54:08 +0530

    Fix DBUtil to read environment variables directly instead of ResourceBundle

--- src/com/shashi/utility/DBUtil.java
+++ src/com/shashi/utility/DBUtil.java
@@ -3,7 +3,6 @@ package com.shashi.utility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
 
 import com.shashi.beans.TrainException;
 import com.shashi.constant.ResponseCode;
@@ -12,24 +11,49 @@ public class DBUtil {
 	private static Connection con;
 
 	static {
-
-		ResourceBundle rb = ResourceBundle.getBundle("application");
+		// Read environment variables with fallback to default values
+		String dbHost = System.getenv("DB_HOST");
+		if (dbHost == null || dbHost.isEmpty()) {
+			dbHost = "db.adozsmkdxblihnaoodiu.supabase.co";
+		}
+		
+		String dbPort = System.getenv("DB_PORT");
+		if (dbPort == null || dbPort.isEmpty()) {
+			dbPort = "5432";
+		}
+		
+		String dbName = System.getenv("DB_NAME");
+		if (dbName == null || dbName.isEmpty()) {
+			dbName = "postgres";
+		}
+		
+		String username = System.getenv("DB_USERNAME");
+		if (username == null || username.isEmpty()) {
+			username = "postgres";
+		}
+		
+		String password = System.getenv("DB_PASSWORD");
+		if (password == null || password.isEmpty()) {
+			password = "Train@Reservation01";
+		}
+		
+		String driverName = "org.postgresql.Driver";
+		String connectionString = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName + "?sslmode=require";
 
 		try {
-			Class.forName(rb.getString("driverName"));
+			Class.forName(driverName);
 		} catch (ClassNotFoundException e) {
+			System.err.println("PostgreSQL Driver not found!");
 			e.printStackTrace();
 		}
 
 		try {
-			System.out.println(rb.getString("connectionString"));
-			System.out.println(rb.getString("username"));
-			System.out.println(rb.getString("password"));
-			con = DriverManager.getConnection(rb.getString("connectionString"), rb.getString("username"),
-					rb.getString("password"));
+			System.out.println("Connecting to: " + connectionString);
+			System.out.println("Username: " + username);
+			con = DriverManager.getConnection(connectionString, username, password);
 			System.out.println("Connection Success!!");
 		} catch (SQLException e) {
-
+			System.err.println("Failed to connect to database!");
 			e.printStackTrace();
 		}
 	}
